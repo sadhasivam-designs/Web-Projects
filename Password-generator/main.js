9a9764eccdbd8bc1328fb6cba5a83a0c691e5b8d
Sadhasivam <62686520+Ishidaaaa@users.noreply.github.com>
2022-05-12 05:06:59 +0530

    Added characters in a single variable

--- Password-generator/main.js
+++ Password-generator/main.js
@@ -1,8 +1,12 @@
-let numbers = [0,1,2,3,4,5,6,7,8,9];
-let specialCharacters = ["!","@","#","$","%","^","&","*","(",")","-","=","/","_","+","{","}","[","]",":",";",",",".","|","?"];
-let capitalAlphabets = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]; 
-let alpha = "qwertyuiopasdfghjklzxcvbnm";
-let smallAlphabets = alpha.split(""); //SPLIT CONVERTS STRING TO CHARACTER ARRAY link="https://www.samanthaming.com/tidbits/83-4-ways-to-convert-string-to-character-array/"
+// let numbers = [0,1,2,3,4,5,6,7,8,9];
+// let specialCharacters = ["!","@","#","$","%","^","&","*","(",")","-","=","/","_","+","{","}","[","]",":",";",",",".","|","?"];
+// let capitalAlphabets = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]; 
+// let alpha = "qwertyuiopasdfghjklzxcvbnm";
+let characters = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&()[]{}?'
+console.log(`The string length ${characters.length}`);
+let userPassLength = document.getElementById("pass-length");
+console.log(userPassLength.value);
+// let smallAlphabets = alpha.split(""); //SPLIT CONVERTS STRING TO CHARACTER ARRAY link="https://www.samanthaming.com/tidbits/83-4-ways-to-convert-string-to-character-array/"
 //TO CHECK THE VALUES OF VARIABLES ABOVE
 // console.log("Values.....")
 // console.log(numbers);
@@ -19,4 +23,16 @@ let smallAlphabets = alpha.split(""); //SPLIT CONVERTS STRING TO CHARACTER ARRAY
 // console.log(specialCharacters.sort());
 // console.log(capitalAlphabets.sort());
 // console.log(smallAlphabets.sort());
-
+function generatePassword(strLength)
+{
+    let result = '';
+    let charactersLength = characters.length;
+    for (let i = 0; i < charactersLength; i++)
+    {  
+        result += characters.charAt((Math.floor(Math.random()*charactersLength)))     
+    }
+    return result;
+}
+console.log("the generated password");
+console.log("-------------------------------------------------------------");
+console.log(generatePassword(10));
