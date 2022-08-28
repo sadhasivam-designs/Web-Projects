cf82753e90d63abdaa0c7eb12255d14b9493d08c
Sadhasivam <iamstarksiv@gmail.com>
2022-08-28 10:11:41 +0530

    added no duplicate function in grab button

--- Browser Extensions/Quick link grabby/main.js
+++ Browser Extensions/Quick link grabby/main.js
@@ -3,7 +3,7 @@ const inputField = document.getElementById("link-input");
 const grabBtn = document.getElementById("grab-btn");
 const delBtn = document.getElementById("del-btn");
 const fetchBtn = document.getElementById("fetch-btn");
-const linkList = document.getElementById("list-items");
+const listOfLink = document.getElementById("list-items");
 let entries = [];
 const entriesInLocalStorage = JSON.parse(localStorage.getItem("entries"))
 if(entriesInLocalStorage)
@@ -11,60 +11,70 @@ if(entriesInLocalStorage)
     entries = entriesInLocalStorage;
     render(entries);
 }
+
+//GRAB BUTTON FUNCTION
+
+grabBtn.addEventListener("click", function(){
+    // let i, flag = 0;
+    chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
+        if(!entries.includes(tabs[0].url))
+        {
+            entries.push(tabs[0].url);
+            localStorage.setItem("entries",JSON.stringify(entries));
+            render(entries);
+        }
+        else
+        {
+            alert("Tab is in the list. No duplicates added!")
+        }     
+    })    
+})
+
+// DELETE BUTTON FUNCTION
+
+delBtn.addEventListener("click", function(){
+    localStorage.clear();
+    entries = [];
+    render(entries);
+    // let i;
+    // for(i=0;i<entries.length;i++)
+    // {
+    //     if(document.getElementById('links-checkbox').checked)
+    //     {
+
+    //         entries[i] == "";
+    //         render(entries);
+    //     }
+    // }
+})
+
+//FETCH BUTTON FUNCTION
+
 fetchBtn.addEventListener("click", function(){
     entries.push(inputField.value);
     inputField.value = "";
     render(entries);
 })
 
-delBtn.addEventListener("click", function(){
-    localStorage.clear();
-    entries = [];
-    render(entries);
-})
+//GROUP BUTTON FUNCTION
 
-grabBtn.addEventListener("click", function(){
-    let i;
-    chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
-        for(i=0;i<tabs.length;i++)
-        {
-            if(tabs[i] != tabs)
-            {
-                entries.push(tabs[0].url);
-                localStorage.setItem("entries",JSON.stringify(entries));
-                render(entries);
-            }
-            else
-            {
-                alert("Already added!");
-            }
-        }
-        
-    })    
-})
-// function grabber()
-// {
-// }
-// function deleteEntries()
-// {
-// }
-// function groupEntries()
-// {
-// }
+
+
+//RENDER OR PRINTING FUNCTION
+
 function render(dummyLink)
 {
     let listItems = "";
     for(let i = 0 ; i < dummyLink.length ; i++ )
     {
         listItems += `
-            <li>
-            <input class="styled-checkbox" id="styled-checkbox-2" type="checkbox">
-            <label for="styled-checkbox-2"> <a target='_blank' href='${dummyLink[i]}'>
-            ${dummyLink[i]}
-        </a></label>
-            </li> 
+            <input class="checkbox" id="links-checkbox" type="checkbox">
+                <label for="links-checkbox">
+                    <a target='_blank' href='${dummyLink[i]}'>
+                        ${dummyLink[i]}
+                    </a>
+                </label><br>
         `
-        // listItems.setAttribute("type", "checkbox");
     }
-    linkList.innerHTML = listItems;
+    listOfLink.innerHTML = listItems;
 }
